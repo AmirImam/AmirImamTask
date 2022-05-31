d09b16ae102058c6ba031e021827422517eebce9
Amir Imam Mohamed <ai_1583@hotmail.com>
2022-05-31 19:59:17 +0300

    Display ItemStores

--- AmirImamTask.AngularUI/src/app/components/item-stores/item-stores.component.ts
+++ AmirImamTask.AngularUI/src/app/components/item-stores/item-stores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Item } from 'src/app/models/Item';
+import { ItemStore } from 'src/app/models/ItemStore';
 import { ApiCallerService } from 'src/app/services/api-caller.service';
 
 @Component({
@@ -14,10 +15,26 @@ export class ItemStoresComponent implements OnInit {
     private Api: ApiCallerService) { }
 
   public ItemModel = new Item();
+  public ItemStoresList = new Array<ItemStore>();
   private Id: string = "";
   public async ngOnInit(): Promise<void> {
     this.Id = this.Path.snapshot.params["Id"];
     this.ItemModel = await this.Api.GetByIdAsync<Item>("Item", this.Id);
+    this.ItemStoresList = await this.Api.GetAllAsync<ItemStore>(`ItemStore/GetItemStoresByItem/${this.Id}`);
   }
 
+  public async AddItemToStore(context: ItemStore) {
+    let itemStore = {
+      ItemId: this.Id,
+      StoreId: context.StoreId
+    };
+
+    let result = await this.Api.PostAsync<{ Id: string }>("ItemStore", itemStore);
+    if (result.Success == true) {
+      context.ItemStoreId = result.Model?.Id as string;
+      context.Quantity = 0;
+      context.ItemId = this.Id;
+
+    }
+  }
 }
--- AmirImamTask.BusinessServices/ItemStores/ItemStoreService.cs
+++ AmirImamTask.BusinessServices/ItemStores/ItemStoreService.cs
@@ -1,4 +1,5 @@
 ﻿using AmirImamTask.Entities.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace AmirImamTask.BusinessServices;
 
@@ -10,21 +11,39 @@ public class ItemStoreService : ServiceBase<ItemStore>, IItemStoreService
 
     public async Task<IEnumerable<ItemStoreDto>> GetItemStoresByItemAsync(Guid itemId)
     {
+        //  string query = $@" SELECT Item.[ItemName], Store.[StoreName], ItemStore.[Quantity]
+        //FROM [Store] 
+        //LEFT JOIN [ItemStore] ON Store.[Id] = ItemStore.[StoreId]
+        //LEFT OUTER JOIN [Item]  ON ItemStore.[ItemId] = Item.[Id] and ItemStore.ItemId = '{itemId}'";
+        //  var result = new DbSet<ItemStoreDto>().FromSqlRaw<ItemStoreDto>(query);
+        //var xcasx = from x in Context.ItemStores
+        //            join y in Context.Items
+        //            on new { X1 = x.ItemId.Value, X2 = x.ItemId.Value } equals new { X1 = y.Id, X2 = itemId }
+        //            select new
+        //            {
+        //                /// Columns
+        //            };
         var result = from store in Context.Stores
-                     join itemStore in Context.ItemStores on store.Id equals itemStore.StoreId into store_itemStore_j
+                     join itemStore in Context.ItemStores 
+                     on new { s1 = store.Id,s2=itemId } equals new { s1 = itemStore.StoreId.Value,s2=itemStore.ItemId.Value } into store_itemStore_j
                      from store_itemStore in store_itemStore_j.DefaultIfEmpty()
-
-                     join item in Context.Items on store_itemStore.ItemId equals item.Id into item_itemStore_j
+                     join item in Context.Items
+                     on new 
+                     { i1 = store_itemStore.ItemId.Value, i2 = store_itemStore.ItemId.Value } 
+                     equals new { i1= item.Id, i2= itemId }
+                     into item_itemStore_j
                      from item_itemStore in item_itemStore_j.DefaultIfEmpty()
-                     where store_itemStore.ItemId == itemId || store_itemStore.ItemId == null
+                     //where store_itemStore.ItemId == itemId || store_itemStore.ItemId == null
                      select new ItemStoreDto
                      {
-                         
+                         ItemId = item_itemStore.Id,
+                         StoreId = store.Id,
+                         ItemStoreId = store_itemStore.Id,
                          ItemName = item_itemStore.ItemName,
                          StoreName = store.StoreName,
                          Quantity = store_itemStore.Quantity
                      };
-                     
+
 
         //var result = from store in Context.Stores
         //               join itemStore in Context.ItemStores 
@@ -38,7 +57,7 @@ public class ItemStoreService : ServiceBase<ItemStore>, IItemStoreService
         //             select new ItemStoreDto
         //             {
         //                 ItemStoreId = store_itemStore.Id,
-                         
+
         //                 ItemId = item_itemStore.Id,
         //                 ItemName = item_itemStore.ItemName,
 
@@ -49,7 +68,7 @@ public class ItemStoreService : ServiceBase<ItemStore>, IItemStoreService
 
         //             };
 
-        
+
         return await result.ToListAsync();
     }
 }
